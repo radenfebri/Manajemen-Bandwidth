03453dea3094d449eff1948f61baf635d93bbda4
Eka Febriansah <febriye12@gmail.com>
2024-05-06 21:18:34 +0700

    Update Notifikasi

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -57,7 +57,6 @@
                 </div>
                 <div class="form-footer">
                     <button type="submit" class="btn btn-primary w-100">
-                        <!-- Download SVG icon from http://tabler-icons.io/i/mail -->
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
@@ -69,7 +68,7 @@
                 </div>
             </div>
         </form>
-        <div class="text-center text-muted mt-3">Forget it, <a href="{{ asset('template') }}index.html">send me back</a> to the sign in screen.</div>
+        <div class="text-center text-muted mt-3">Forget it, <a href="{{ route('login') }}">send me back</a> to the sign in screen.</div>
     </div>
 </div>
 
